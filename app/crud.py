b1e865cf155057eaacdf881869801ba7a4d4110d
HUAMAN CASTILLO JHON ALEXIS <jhon.huamanc@unmsm.edu.pe>
2026-04-26 23:26:20 -0500

    Semana 4 : fix middleware

--- app/crud.py
+++ app/crud.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # app/crud.py
 from sqlalchemy.orm import Session
 from app import models, schemas
@@ -85,3 +86,40 @@ def obtener_estadisticas_globales(db: Session):
         "estacion_punto_critico": punto_critico_id,
         "valor_maximo": valor_maximo
     }
+=======
+from sqlalchemy.orm import Session
+import models,schemas
+
+#1.Gestion de infraestructura
+def crear_estacion(estacion: EstacionCreate, db: Session = Depends(get_db)):
+    # Convertimos el esquema de Pydantic a Modelo de SQLAlchemy
+    nueva_estacion = models.EstacionDB(id=estacion.id, nombre=estacion.nombre,
+ubicacion=estacion.ubicacion)
+    db.add(nueva_estacion)
+    db.commit()
+    db.refresh(nueva_estacion)
+    return {"msj": "Estación guardada en DB", "data": nueva_estacion}
+
+def obtner_estacion(db:Session,estacion_id:int):
+        return
+db.query(models.EstacionDB).filter(models.EstacionDB.id==estacion_id).fisrt()
+
+def listar_estaciones(db: Session ):
+    # Esta línea hace la consulta SQL: SELECT * FROM estaciones;
+    return db.query(models.EstacionDB).all()
+
+# 2.Telemetria de sensores
+
+def registrar_lectura(lectura: LecturaCreate, db: Session = Depends(get_db)):
+    # Validar si la estación existe en la DB
+    estacion = db.query(models.EstacionDB).filter(models.EstacionDB.id ==
+lectura.estacion_id).first()
+    if not estacion:
+        raise HTTPException(status_code=404, detail="Estación no existe")
+
+    nueva_lectura = models.LecturaDB(valor=lectura.valor,
+estacion_id=lectura.estacion_id)
+    db.add(nueva_lectura)
+    db.commit()
+    return {"status": "Lectura guardada en DB"}
+>>>>>>> 22179523fa1522c3e634baeda63fd0eb3f454aa9
--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from sqlalchemy import Column, Integer, String, Float, ForeignKey
 from sqlalchemy.orm import relationship
 from app.database import Base
@@ -16,3 +17,23 @@ class LecturaDB(Base):
     estacion_id = Column(Integer, ForeignKey("estaciones.id"))
     
     estacion = relationship("EstacionDB", back_populates="lecturas")
+=======
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import relationship
+from database import Base
+class EstacionDB(Base):
+    __tablename__ = "estaciones"
+    id = Column(Integer, primary_key=True, index=True)
+    nombre = Column(String)
+    ubicacion = Column(String)
+    # Relación: permite acceder a las lecturas desde el objeto estación
+    lecturas = relationship("LecturaDB", back_populates="estacion")
+
+class LecturaDB(Base):
+    __tablename__ = "lecturas"
+    id = Column(Integer, primary_key=True, index=True)
+    valor = Column(Float)
+    estacion_id = Column(Integer, ForeignKey("estaciones.id"))
+    
+    estacion = relationship("EstacionDB", back_populates="lecturas")
+>>>>>>> 22179523fa1522c3e634baeda63fd0eb3f454aa9
